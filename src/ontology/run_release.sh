bcea2f575074005f64652ebadf08da8b3eb794c3
Clare72 <cp390@cam.ac.uk>
2026-04-03 16:08:43 +0100

    Update run_release.sh

--- src/ontology/run_release.sh
+++ src/ontology/run_release.sh
@@ -3,10 +3,10 @@
 set -e
 
 # 1. Preprocessing - not required for FBdv. To use this, copy code for pre_release from FBcv.Makefile
-#sh run.sh make pre_release -B
+#sh run.sh make pre_release
 
 # 2. Proper release.
-sh run.sh make prepare_release -B
+sh run.sh make prepare_release
 # To use auto-generated definitions, copy pre_release code from FBcv and use
-#sh run.sh make SRC=fbdv-edit-release.owl IMP=false PAT=false prepare_release -B
+#sh run.sh make SRC=fbdv-edit-release.owl IMP=false PAT=false prepare_release
 
